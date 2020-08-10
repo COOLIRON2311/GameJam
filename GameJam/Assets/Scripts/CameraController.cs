a12dffbd4b5898e89452cf491bcf807c3acb9f2a
caisilus <serangor@gmail.com>
2020-08-10 20:09:19 +0300

    added some camera interactions

---
+++ GameJam/Assets/Scripts/CameraController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    private float z;
+    
+    private GameObject following;
+
+    private void Start()
+    {
+        z = transform.position.z;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = new Vector3(following.transform.position.x, following.transform.position.y, z);
+    }
+
+    public void SetFollowing(GameObject gameObject)
+    {
+        following = gameObject;
+    }
+}
---
+++ GameJam/Assets/Scripts/FireSpawner.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FireSpawner : MonoBehaviour
+{
+
+    [SerializeField] private GameObject fire;
+    [SerializeField] private Camera camera;
+    private GameObject _fire;
+    private CameraController _cameraController;
+    
+    // Update is called once per frame
+
+    void Start()
+    {
+        _cameraController = camera.GetComponent<CameraController>();
+        _cameraController.SetFollowing(this.gameObject);
+    }
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            if (_fire == null)
+                _fire = Instantiate(fire);
+            _fire.transform.position = transform.position;
+            _cameraController.SetFollowing(_fire);
+        }
+
+        if (Input.GetMouseButtonDown(1))
+        {
+            if (_fire != null)
+                Destroy(_fire.gameObject);
+            _cameraController.SetFollowing(this.gameObject);
+        }
+    }
+}
