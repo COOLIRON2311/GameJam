236c01ab3d974714d3ac7313ac685c3b01f46eb1
caisilus <serangor@gmail.com>
2020-08-10 17:58:12 +0300

    added simple platformer controls (only moving, no jumps)

---
+++ GameJam/Assets/Scripts/PlayerControl.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControl : MonoBehaviour
+{
+    public float speed = 3.0f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        float deltaX = Input.GetAxis("Horizontal");
+        this.transform.Translate(deltaX * speed * Time.deltaTime, 0, 0);
+    }
+}
