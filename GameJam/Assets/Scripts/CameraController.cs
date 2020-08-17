56fd043ca992bd9428a9375c30d02cc976e569bc
caisilus <serangor@gmail.com>
2020-08-17 17:23:59 +0300

    camera borders beta

--- GameJam/Assets/Scripts/CameraController.cs
+++ GameJam/Assets/Scripts/CameraController.cs
@@ -8,13 +8,17 @@ public class CameraController : MonoBehaviour
     private float z;
     
     private GameObject following;
+    private Rigidbody2D _rigidbody2D;
 
     public bool isFree;
+    public bool isInBorder;
     public float moveSpeed = 0.1f;
 
     private void Start()
     {
+        _rigidbody2D = GetComponent<Rigidbody2D>();
         isFree = false;
+        isInBorder = true;
         z = transform.position.z;
     }
 
@@ -43,8 +47,18 @@ public class CameraController : MonoBehaviour
             {
                 deltaX = -moveSpeed;
             }
-            
-            transform.Translate(deltaX, deltaY, 0);
+
+            if (isInBorder)
+            {
+                _rigidbody2D.velocity = new Vector2(deltaX, deltaY);    
+            }
+            else
+            {
+                _rigidbody2D.velocity = Vector2.zero;
+                _rigidbody2D.position = _rigidbody2D.position - 
+                                        new Vector2(deltaX, deltaY);
+                isInBorder = true;
+            }
         }
         else
         {
