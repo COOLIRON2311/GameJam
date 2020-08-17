f1ad337513c3c18014b382dab0b59fb71214c26d
caisilus <serangor@gmail.com>
2020-08-17 17:24:44 +0300

    idiot commit

---
+++ GameJam/Assets/Scripts/CameraBorderController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraBorderController : MonoBehaviour
+{
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        Debug.Log($"{other} trigger exit");
+        CameraController cameraController = other.GetComponent<CameraController>();
+        if (cameraController != null)
+        {
+            cameraController.isInBorder = false;
+        }
+    }
+}
